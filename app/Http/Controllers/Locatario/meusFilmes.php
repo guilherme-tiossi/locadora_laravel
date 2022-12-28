d792e9734f704757b34670e5e7e6a501ae306ef5
guilherme-tiossi <guilherme.a.m.tiossi@gmail.com>
2022-12-28 12:05:42 -0300

    Especificação de que filmes listados na meus-filmes devem pertencer ao usuário  logado

--- app/Http/Controllers/Locatario/meusFilmes.php
+++ app/Http/Controllers/Locatario/meusFilmes.php
@@ -6,13 +6,16 @@ use App\Models\AluguelFilmeModel;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
 
 class meusFilmes extends Controller
 {
     public function listarmeusfilmes(){
+        $user_id = Auth::user()->id;
         $alugueis = DB::table('tbalugueis')
-        ->join('tbfilmes', 'tbalugueis.id_filme', '=', 'tbfilmes.id_filme',)
+        ->join('tbfilmes', 'tbalugueis.id_filme', '=', 'tbfilmes.id_filme')
         ->select('tbalugueis.id_filme', 'tbfilmes.titulo_filme')
+        ->where('tbalugueis.id_user', '=', $user_id)
         ->get();
         return view('meus_filmes', compact('alugueis'));
     }
