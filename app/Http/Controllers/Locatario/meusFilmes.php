f744abcdfc1a6a340d0b4a0435b2c0c5c1dd5a10
guilherme-tiossi <guilherme.a.m.tiossi@gmail.com>
2023-01-06 14:00:04 -0300

    Adicionado sistema de devolução automático

--- app/Http/Controllers/Locatario/meusFilmes.php
+++ app/Http/Controllers/Locatario/meusFilmes.php
@@ -14,9 +14,30 @@ class meusFilmes extends Controller
         $user_id = Auth::user()->id;
         $alugueis = DB::table('tbalugueis')
         ->join('tbfilmes', 'tbalugueis.id_filme', '=', 'tbfilmes.id_filme')
-        ->select('tbalugueis.id_filme', 'tbfilmes.titulo_filme')
+        ->select('tbalugueis.id_filme', 'tbfilmes.titulo_filme', 'tbalugueis.validade_aluguel')
         ->where('tbalugueis.id_user', '=', $user_id)
         ->get();
+        $data = date("Y-m-d");
+        foreach($alugueis as $f){
+            $validade = $f->validade_aluguel;
+            //dd($data . " > " . $validade);
+
+            if ($data > $validade){
+                $disponiveis_filme = DB::table('tbfilmes')->where('id_filme', '=', '1')->value('disponiveis_filme');
+                $user_id = Auth::user()->id;
+                ++$disponiveis_filme;
+                DB::table('tbalugueis')
+                ->where([
+                    ['id_filme', '=', $f->id_filme],
+                    ['id_user', '=', $user_id],
+                ])
+                ->delete();
+                DB::table('tbfilmes')
+                ->where('id_filme', $f->id_filme)
+                ->update(['disponiveis_filme' => $disponiveis_filme]);
+            }
+        }
+
         return view('meus_filmes', compact('alugueis'));
     }
 }
