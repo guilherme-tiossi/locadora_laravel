16edaa3a47b65b9ee614a1563237240f13a0040d
guilherme-tiossi <guilherme.a.m.tiossi@gmail.com>
2023-01-12 01:38:37 -0300

    Adicionado rotina de cálculo de lucros mensais baseada no aluguel de filmes por X ano

--- app/Http/Controllers/financeiroController.php
+++ app/Http/Controllers/financeiroController.php
@@ -14,9 +14,49 @@ class financeiroController extends Controller
         ->select('tbalugueis.validade_aluguel', 'tbalugueis.valor_filme')
         ->get();
         $lucros_totais = 0;
+        $jan = 0; $jan = 0; $jan = 0; $fev = 0; $mar = 0; $abr = 0; $mai = 0; $jun = 0; $jul = 0; $ago = 0; $set = 0; $out = 0; $nov = 0; $dez = 0; 
+        $ano = 2023;
         foreach ($alugueis_totais as $lt){
+            switch ($lt->validade_aluguel){
+               case(str_contains($lt->validade_aluguel, $ano. '-01')):
+                   $jan = $jan + $lt->valor_filme;
+               break;
+               case(str_contains($lt->validade_aluguel, $ano. '-02')):
+                   $fev = $fev + $lt->valor_filme;
+               break;
+               case(str_contains($lt->validade_aluguel, $ano. '-03')):
+                   $mar = $mar + $lt->valor_filme;
+               break;
+               case(str_contains($lt->validade_aluguel, $ano. '-04')):
+                   $abr = $abr + $lt->valor_filme;
+               break;
+               case(str_contains($lt->validade_aluguel, $ano. '-05')):
+                   $mai = $mai + $lt->valor_filme;
+               break;
+               case(str_contains($lt->validade_aluguel, $ano. '-06')):
+                   $jun = $jun + $lt->valor_filme;
+               break;
+               case(str_contains($lt->validade_aluguel, $ano. '-07')):
+                   $jul = $jul + $lt->valor_filme;
+               break;
+               case(str_contains($lt->validade_aluguel, $ano. '-08')):
+                   $ago = $ago + $lt->valor_filme;
+               break;
+               case(str_contains($lt->validade_aluguel, $ano. '-09')):
+                   $set = $set + $lt->valor_filme;
+               break;
+               case(str_contains($lt->validade_aluguel, $ano. '-10')):
+                   $out = $out + $lt->valor_filme;
+               break;
+               case(str_contains($lt->validade_aluguel, $ano. '-11')):
+                   $nov = $nov + $lt->valor_filme;
+               break;
+               case(str_contains($lt->validade_aluguel, $ano. '-12')):
+                   $dez = $dez + $lt->valor_filme;
+               break;
+            }
             $lucros_totais = $lucros_totais + $lt->valor_filme;
         }
-        return view('dashboard_adm', compact('alugueis_totais', 'lucros_totais'));
+        return view('dashboard_adm', compact('alugueis_totais', 'lucros_totais', 'jan', 'fev', 'mar', 'abr', 'mai', 'jun', 'jul', 'ago', 'set', 'out', 'nov', 'dez'));
     }
 }
--- resources/views/dashboard_adm.blade.php
+++ resources/views/dashboard_adm.blade.php
@@ -1,7 +1,14 @@
 @include('includes.head')
 @include('includes.header_adm')
-@foreach ($alugueis_totais as $lt)
-    <p> {{$lt->valor_filme}} </p> <br>
-@endforeach
-
-<h2> {{$lucros_totais}} </h1>
+<p> Janeiro: {{$jan}} <p>
+<p> Fevereiro: {{$fev}} <p>
+<p> Março: {{$mar}} <p>
+<p> Abril: {{$abr}} <p>
+<p> Maio: {{$mai}} <p>
+<p> Junho: {{$jun}} <p>
+<p> Julho: {{$jul}} <p>
+<p> Agosto: {{$ago}} <p>
+<p> Setembro: {{$set}} <p>
+<p> Outubro: {{$out}} <p>
+<p> Novembro: {{$nov}} <p>
+<p> Dezembro: {{$dez}} <p>
