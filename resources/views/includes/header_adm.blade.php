32b13ce0c8a78c8a5ebd9ab3c654a259496dae34
guilherme-tiossi <guilherme.a.m.tiossi@gmail.com>
2023-01-23 16:47:21 -0300

    Mudanças na estilização do menu do ADM

--- resources/views/includes/header_adm.blade.php
+++ resources/views/includes/header_adm.blade.php
@@ -3,14 +3,8 @@
     <nav class="menu-topo">
         <div style="display: flex;">
             <h2 class="titulo-menu"> ADM </h2>
-            <form class="form-pesquisa">
-                <input type="text" name="pesquisa" class="input-pesquisa">
-                <button type="submit" name="pesquisar" class="btn-pesquisa">
-                    <i class="fa-solid fa-magnifying-glass"></i>
-                </button>
-            </form>
         <div class="div-center">
-        <ul class="ul-menu">
+        <ul class="ul-menu-adm">
             <li class="lista-menu"> <a class="link-menu" href="/dashboard_adm"> Estatísticas </a> </li>
             <li class="lista-menu"> <a class="link-menu" href="/empregados_adm"> Relações empregatícias </a> </li>
             <li class="lista-menu"> <a class="link-menu" href="/crud_adm"> Adicionar filmes e gêneros </a> </li>
